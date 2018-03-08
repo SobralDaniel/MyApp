05ac0c5450b69b7d7f8521295ebdd286ac89b35f
nbacademy <nbacademy@LT127333.novabase.intra>
2018-03-08 17:20:25 +0000

    Initial database commit

---
+++ app/src/main/java/com/example/nbacademy/myapp/database/DBHelper.java
@@ -0,0 +1,34 @@
+package com.example.nbacademy.myapp.database;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by nbacademy on 08/03/2018.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("QUERIES DE CRIACAO DE TODAS AS TABELAS");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("QUERIES PARA APAGAR AS TABELAS");
+        //VER SE FAZ SENTIDO USAR O OLDVERSION E O NEWVERSION
+        this.onCreate(db);
+    }
+}
