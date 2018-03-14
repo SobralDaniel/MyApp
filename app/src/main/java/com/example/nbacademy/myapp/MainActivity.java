ebfdf6cc29027f12a58b108b9c34e9798f9ce690
bdhmedinas <bdhmedinas@gmail.com>
2018-03-14 15:34:32 +0000

    pre-merge

--- app/src/main/java/com/example/nbacademy/myapp/MainActivity.java
+++ app/src/main/java/com/example/nbacademy/myapp/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+ //       setSupportActionBar(toolbar);
 
         ImageView addTrip = (ImageView) findViewById(R.id.addTrip);
 
